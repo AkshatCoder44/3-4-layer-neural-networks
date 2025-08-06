2b961f731aa20d9844a2f4d6ac6b6f4f65ac8698
Akshat Sahay <106474056+AkshatCoder44@users.noreply.github.com>
2025-08-06 22:29:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ let b2 = Math.random();
 let w3 = Math.random();
 let b3 = Math.random();
 
-let input = 1;
-let target = 0.9;
+let input = 0.6;
+let target = 1.0;
 let lr = 0.1;
 let e = 10000;
 
@@ -25,23 +25,23 @@ for(let i = 0; e > i; i++) {
   let z1 = w1 * input + b1;
   let h1 = sig(z1);
   
-  let zo = w2 * h1 + b2;
-  let output0 = sig(zo);
-  let h2 = w3 * output0 + b3;
-  let output = sig(h2);
+  let z2 = w2 * h1 + b2;
+  let h2 = sig(z2);
+  let z3 = w3 * h2 + b3;
+  let output = sig(z3);
   let error = output - target;
-  let dout = error * sigD(h2);
+  let dout = error * sigD(z3);
   
-  w3 -= lr * dout * output0;
+  w3 -= lr * dout * h2;
   b3 -= lr * dout;
   
-  let dh1 = dout * w3 * sigD(zo);
-  w2 -= lr * dh1 * h1;
-  b2 -= lr * dh1;
+  let dh2 = dout * w3 * sigD(z2);
+  w2 -= lr * dh2 * h1;
+  b2 -= lr * dh2;
   
-  let dh2 = dout * w3 * sigD(zo) * w2 * sigD(z1);;
-  w1 -= lr * dh2 * input;
-  b1 -= lr * dh2;
+  let dh1 = dh2 * w2 * sigD(z1);;
+  w1 -= lr * dh1 * input;
+  b1 -= lr * dh1;
 }
 
-console.log(sig(w3 * sig(w2 * sig(w1 * input + b1) + b2) + b3));
+console.log(sig(w3 * sig(w2 * sig(w1 * input + b1) + b2) + b3).toFixed(2));
